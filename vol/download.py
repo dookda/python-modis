6506410ee954e6a6f7bcb92dbaea03eb4fdf4715
dookda <sakda.homhuan@gmail.com>
2021-11-03 12:06:35 +0700

    update config

--- vol/download.py
+++ vol/download.py
@@ -1,29 +1,37 @@
 import os
 import subprocess
 from auth import earth
-
+from geo.Geoserver import Geoserver
 
 username = earth["user"]
 password = earth["pass"]
 
-print("downloading...")
-# cmd = f"modis_download.py -U {username} -P {password} -I -r -t h18v03,h18v04 -f 2008-01-01 -e 2008-01-31 /mnt/lst_terra/"
-cmd = f"modis_download.py -U {username} -P {password} -r -s MOLA -p MYD09A1.006 -t h27v07 -f 2010-01-01 -e 2010-12-31 lst_aqua/"
-# os.system(cmd)
+path = 'lst_terra/'
 
-dir = os.listdir('lst_terra/')
-for f in dir:
-    # print(f)
-    if f.endswith(".hdf"):
-        a = f.split(".")
-        # print(a)
-        outtrans = f"{a[0]}_{a[1]}_{a[2]}_{a[3]}_{a[4]}_trans.tif"
-        cmd = f"gdal_translate -sds lst_terra/{f} {out}"
-        os.system(cmd)
-        print(cmd)
+geo = Geoserver('http://127.0.0.1:8080/geoserver',
+                username='admin', password='geoserver')
 
-        outwarp = f"{a[0]}_{a[1]}_{a[2]}_{a[3]}_{a[4]}_warp.tif"
-        cmd = f"gdalwarp - overwrite {outtrans} {outwarp} - t_srs EPSG: 32647"
-        os.system(cmd)
 
-print("ok")
+def warp():
+    dir = os.listdir(path)
+    for f in dir:
+        if f.endswith(".hdf"):
+            a = f.split(".")
+            # print(a)
+            out = f"{a[0]}_{a[1]}_{a[2]}_{a[3]}_{a[4]}"
+            cmd = f'modis_convert.py -s "( 1 0 0 0 1 0 0 0 0 0 0 0 )" -o {path}{out} -e 32647  {path}{f}'
+            os.system(cmd)
+            print("finish warp")
+
+
+def download():
+    print("downloading...")
+    # cmd = f"modis_download.py -U {username} -P {password} -I -r -t h18v03,h18v04 -f 2008-01-01 -e 2008-01-31 /mnt/lst_terra/"
+    cmd = f"modis_download.py -U {username} -P {password} -r -s MOLA -p MYD11A1.006 -t h27v07 -f 2021-01-01 -e 2021-01-02 {path}"
+    # os.system(cmd)
+    print("finish download")
+    # warp()
+
+
+if __name__ == '__main__':
+    download()
